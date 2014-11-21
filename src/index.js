171d75227795f385569b5283db944b792369b18c
Ben Drucker <bvdrucker@gmail.com>
2014-11-21 16:41:06 -0500

    refactor(parser): move header parser out to separate fn

--- src/index.js
+++ src/index.js
@@ -5,17 +5,21 @@ var util = require('util');
 // https://github.com/angular/angular.js/blob/master/validate-commit-msg.js#L19
 var pattern = /^(?:fixup!\s*)?(\w*)(\(([\w\$\.\*/-]*)\))?\: (.*)$/;
 
+function header (line) {
+  var matches = pattern.exec(line);
+  return {
+    type: matches[1],
+    scope: matches[3],
+    subject: matches[4],
+    toString: function () {
+      return util.format('%s(%s): %s', this.type, this.scope, this.subject);
+    }
+  };
+}
+
 exports.parse = function parseMessage (message) {
   var lines = message.split('\n');
-  var matches = pattern.exec(lines[0]);
   return {
-    header: {
-      type: matches[1],
-      scope: matches[3],
-      subject: matches[4],
-      toString: function () {
-        return util.format('%s(%s): %s', this.type, this.scope, this.subject);
-      }
-    }
-  }
+    header: header(lines[0])
+  };
 };
